e950fda1692b8c7288980d0d99897216adf441cb
ratul <anfal15-14303@diu.edu.bd>
2022-04-14 06:32:17 +0600

    init

---
+++ order_form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Order Form </title>
+</head>
+<body>
+    <form method="post" target="_self">
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="number" name="quantity" placeholder="Quantity" required>
+        <input type="submit" value="Go!">
+    </form>
+</body>
+</html>
